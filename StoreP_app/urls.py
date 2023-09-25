e621fcbfa412fb751ffabff7c9a0e678ed5aafc6
ruchi-singh0509 <123281835+ruchi-singh0509@users.noreply.github.com>
2023-09-25 07:27:55 +0530

    Update urls.py

--- StoreP_app/urls.py
+++ StoreP_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework import routers
-from StoreP_app.views import StoreViewSet
+from StoreP_app.views import RestroViewSet
 
 default_router = routers.SimpleRouter()
-default_router.register("store", StoreViewSet, basename="store")
+default_router.register("restro", RestroViewSet, basename="restro")
 
 urlpatterns = default_router.urls

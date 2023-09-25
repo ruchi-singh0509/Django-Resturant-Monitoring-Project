cced9376c2273fb84d4bbd784014f258b3e39a63
ruchi-singh0509 <123281835+ruchi-singh0509@users.noreply.github.com>
2023-09-25 07:28:30 +0530

    Update views.py

--- StoreP_app/views.py
+++ StoreP_app/views.py
@@ -1,40 +1,41 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from .models import Store_Timezone, Report
-from .serializers import Report_Serializer
 from rest_framework.response import Response
-from StoreP_app.populate_data import generate_report, generate_csv_file, trigger_report
+from rest_framework.generics import get_object_or_404
+from StoreP_app.models import Store_Timezone, Report, report_status
+from .serializers import Report_Serializer
+from StoreP_app.populate_data import generate_report, generate_csv_file
+from .populate_data import trigger_report
 from django.conf import settings
 
-import pdb
 
-pdb.set_trace()
-
-
-class StoreViewSet(ModelViewSet):
+class RestroViewSet(ModelViewSet):
     queryset = Store_Timezone.objects.all()
+    serializer_class = Report_Serializer
 
-    @action(detail=False, methods=["GET"], url_path="trigger_report")
+    @action(
+        detail=True,
+        methods=["GET"],
+    )
     def trigger_report(self, request, pk=None):
-        store = self.get_object()
-        report = Report.objects.create(store=store, status="Pending")
+        restro = self.get_object()
+        report = Report.objects.create(restro=restro, status=report_status.PENDING)
         csv_data = []
-        data = generate_report(store)
+        data = generate_report(restro)
         csv_data.append(data)
         generate_csv_file(report, csv_data)
-        return Response(status=200, data={"report_id": report.pk})
+        return Response(status=200, data={"report_id": report.id})
 
-    @action(detail=False, methods=["GET"], url_path="get_report")
+    @action(
+        detail=False,
+        methods=["POST"],
+    )
     def get_report(self, request):
         report_id = request.data.get("report_id")
-        report = Report.objects.get(pk=report_id)
-
-        serializer = Report_Serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        report = Report.DoesNotExist(Report, id=report_id)
-        if report.status == Report.report_status.COMPLETED:
-            report_url = Report.report_url.url
-            with open(report_url.path, "rb") as csv_file:
+        report = get_object_or_404(Report, id=report_id)
+        if report.status == report_status.COMPLETED:
+            report_url = settings.MEDIA_ROOT + "/" + report.report_url.name
+            with open(report_url, "rb") as csv_file:
                 csv_data = csv_file.readlines()
                 return Response(
                     status=200,
@@ -48,6 +49,6 @@ class StoreViewSet(ModelViewSet):
             return Response(status=200, data={"status": "Running"})
 
     def get_serializer_class(self):
-        if self.action == "get_report":
+        if self.action == "trigger_report":
             return Report_Serializer
         return super().get_serializer_class()

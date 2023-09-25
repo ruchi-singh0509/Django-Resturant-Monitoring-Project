3e99eee2379a53485f3ea0349020f6ac7d89426a
ruchi-singh0509 <123281835+ruchi-singh0509@users.noreply.github.com>
2023-09-25 07:27:07 +0530

    Update serializers.py

--- StoreP_app/serializers.py
+++ StoreP_app/serializers.py
@@ -1,28 +1,12 @@
 from rest_framework import serializers
-from .models import Store_status, Store_Business_Hour, Store_Timezone, Report
+from StoreP_app.models import Report
+
+__all__ = ["Report_Serializer"]
 
 
 class Report_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Report
-        fields = ("store", "report_status", "report_url")
+        fields = ["report_id"]
 
     report_id = serializers.IntegerField()
-
-
-class Store_Timezone_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Store_Timezone
-        fields = ("store_id", "timezone_str")
-
-
-class Store_status_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Store_status
-        fields = ("store", "status", "timestamp_utc")
-
-
-class Store_Business_Hour_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Store_Business_Hour
-        fields = ("store", "dayOfWeek", "start_time_local", "end_time_local")

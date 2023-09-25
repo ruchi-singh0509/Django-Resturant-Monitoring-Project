e5f0b7572d8649c47f78934bd6087dae133ae6d8
ruchi-singh0509 <123281835+ruchi-singh0509@users.noreply.github.com>
2023-09-25 07:23:31 +0530

    Update admin.py

--- StoreP_app/admin.py
+++ StoreP_app/admin.py
@@ -1,31 +1,41 @@
 from django.contrib import admin
-from .models import Store_status, Store_Business_Hour, Store_Timezone,Report
+from .models import Store_status, Store_Business_Hour, Store_Timezone, Report
 
 # Register your models here.
 admin.site.register(Store_Timezone)
+
+
 class Store_Timezone_Admin(admin.ModelAdmin):
-    list_display = ("store_id","timezone")
-    list_filter=("timezone",)
-    search_field=("store_id",)
+    list_display = ("store_id", "timezone_str")
+    list_filter = ("timezone_str",)
+    search_field = ("store_id",)
+
+
 admin.site.register(Store_Business_Hour)
+
+
 class Store_Business_Hour_Admin(admin.ModelAdmin):
-    list_display=("store","dayOfWeek","start_time","end_time")
-    raw_id_fields= ("store",)
-    list_filter=("dayOfWeek",)
-    search_field=("store_id",)
+    list_display = ("restro", "dayOfWeek", "start_time", "end_time")
+    raw_id_fields = ("restro",)
+    list_filter = ("dayOfWeek",)
+    search_field = ("store_id",)
+
 
 admin.site.register(Store_status)
+
+
 class Store_status_Admin(admin.ModelAdmin):
-    list_display = ("store","status","timestamp_utc")
-    raw_id_fields =("store",)
+    list_display = ("restro", "status", "timestamp_utc")
+    raw_id_fields = ("restro",)
     list_filter = ("status",)
     search_field = ("store_id",)
 
+
 admin.site.register(Report)
+
+
 class Report_Admin(admin.ModelAdmin):
-    list_display=("store","report_status","report_url")
-    raw_id_fields=("store",)
-    list_filter=("report_status")
-    search_field=("store_id")
-
-
+    list_display = ("restro", "status", "report_url")
+    raw_id_fields = ("restro",)
+    list_filter = ("status",)
+    search_field = ("store_id",)
